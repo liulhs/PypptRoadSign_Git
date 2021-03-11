c090770c47ba4c4fac94a71fa616f1989b56657c
Haosong Liu <liulhs1998@gmail.com>
2021-03-11 16:49:06 +0800

    Version fixed

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='PypptRoadSign',
     packages=['PypptRoadSign'],
-    version='0.1',
+    version='0.0.1',
     license='MIT',
     description=' QXWZ Power Point Road Sign Project',
     author='Haosong Liu',
     author_email='haosongliuofficial@gmail.com',
     url='https://github.com/liulhs/PypptRoadSign_Git',
-    download_url='https://github.com/liulhs/PypptRoadSign_Git/archive/v_0.1.tar.gz',
+    download_url='https://github.com/liulhs/PypptRoadSign_Git/archive/v_0.0.1.tar.gz',
     keywords=['Roadsign', 'PowerPoint', 'Genertor'],
     install_requires=[
         'python-pptx'
